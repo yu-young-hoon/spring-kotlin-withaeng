8a98f8029983026113cdc760e29f3d15b03b63ba
Jaemin.Park <once29@naver.com>
2024-02-08 00:51:05 +0900

    Implement BaseEntity

--- withaeng-domain/build.gradle.kts
+++ withaeng-domain/build.gradle.kts
@@ -1,3 +1,9 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
