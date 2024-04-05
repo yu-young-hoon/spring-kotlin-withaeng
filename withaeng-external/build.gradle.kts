686a8ee8215739b7fe76c5a4941009d786b2f506
Jaemin.Park <once29@naver.com>
2024-04-05 18:01:24 +0900

    Add ThymeleafTemplate Bean

--- withaeng-external/build.gradle.kts
+++ withaeng-external/build.gradle.kts
@@ -1,19 +1,3 @@
-plugins {
-    id("java")
-}
-
-group = "org.example"
-version = "0.0.1"
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 }
