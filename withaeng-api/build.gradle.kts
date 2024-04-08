ff4991ca8d3562fa80b5c819f61891164ee183ab
Jaemin.Park <once29@naver.com>
2024-04-08 13:06:49 +0900

    Implement SecurityConfig

--- withaeng-api/build.gradle.kts
+++ withaeng-api/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation(project(":withaeng-domain"))
     implementation(project(":withaeng-common"))
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
 
     // jwt
     implementation("io.jsonwebtoken:jjwt-api:$jjwtVersion")
