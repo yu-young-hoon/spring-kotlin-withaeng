902a09525b8e317623f054b983bfb18150e4518d
이윤선 <yoonseon3@gmail.com>
2024-08-21 03:22:45 +0900

    Chore: 동행 엔티티 @Comment 추가

--- withaeng-domain/src/main/kotlin/com/travel/withaeng/domain/accompany/Accompany.kt
+++ withaeng-domain/src/main/kotlin/com/travel/withaeng/domain/accompany/Accompany.kt
@@ -3,51 +3,63 @@ package com.travel.withaeng.domain.accompany
 import com.travel.withaeng.converter.TagIdsConverter
 import com.travel.withaeng.domain.BaseEntity
 import jakarta.persistence.*
+import org.hibernate.annotations.Comment
 import org.hibernate.annotations.DynamicUpdate
 import java.time.LocalDate
 
 @DynamicUpdate
-@Table(name = "accompany")
 @Entity
+@Table(name = "accompany")
 class Accompany(
 
     @Column(name = "user_id", nullable = false)
     val userId: Long,
 
     @Column(name = "title", nullable = false)
+    @Comment("동행 제목")
     var title: String,
 
     @Lob
     @Column(name = "content", nullable = false)
+    @Comment("동행 내용")
     var content: String,
 
     @Enumerated(EnumType.STRING)
     @Column(name = "accompany_status", nullable = false)
+    @Comment("동행 모집 상태")
     var accompanyStatus: AccompanyStatus = AccompanyStatus.ING,
 
     @Column(name = "start_trip_date", nullable = false)
+    @Comment("여행 시작 일자")
     var startTripDate: LocalDate,
 
     @Column(name = "end_trip_date", nullable = false)
+    @Comment("여행 종료 일자")
     var endTripDate: LocalDate,
 
     @Column(name = "banner_image_url")
+    @Comment("베너 이미지 URI")
     var bannerImageUrl: String?,
 
     @Column(name = "member_count", nullable = false)
+    @Comment("모집 인원")
     var memberCount: Long = 0L,
 
     @Column(name = "view_count", nullable = false)
+    @Comment("조회수")
     var viewCount: Long = 0L,
 
     @Column(name = "open_kakao_url", nullable = false)
+    @Comment("오픈 카카오 채팅 URI")
     var openKakaoUrl: String,
 
     @Embedded
+    @Comment("동행 장소 정보")
     var accompanyDestination: AccompanyDestination,
 
     @Convert(converter = TagIdsConverter::class)
     @Column(name = "tag_ids", nullable = false)
+    @Comment("태그 목록")
     var tagIds: Set<Long> = setOf()
 
 ) : BaseEntity() {
