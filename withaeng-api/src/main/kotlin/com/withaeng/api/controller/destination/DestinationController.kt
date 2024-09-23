b4cb5356147edf6a574032088548fa7c0fd7ee16
안영길 <nilgil183@gmail.com>
2024-09-23 20:51:44 +0900

    refactor: 불필요한 레이어 제거

--- withaeng-api/src/main/kotlin/com/withaeng/api/controller/destination/DestinationController.kt
+++ withaeng-api/src/main/kotlin/com/withaeng/api/controller/destination/DestinationController.kt
@@ -1,22 +1,42 @@
 package com.withaeng.api.controller.destination
 
 import com.withaeng.api.applicationservice.accompany.dto.DestinationResponse
-import com.withaeng.api.applicationservice.destination.DestinationApplicationService
 import com.withaeng.api.common.ApiResponse
+import com.withaeng.domain.destination.City
+import com.withaeng.domain.destination.Continent
+import com.withaeng.domain.destination.Country
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @Tag(name = "Destination", description = "여행지 API")
 @RestController
 @RequestMapping("/api/v1/destinations")
-class DestinationController(private val destinationApplicationService: DestinationApplicationService) {
+class DestinationController {
 
     @GetMapping
     fun getDestinations(): ApiResponse<DestinationResponse> {
         return ApiResponse.success(
-            destinationApplicationService.getDestinations()
+            DestinationResponse(createDestinationsMap())
         )
+    }
+
+    private fun createDestinationsMap(): EnumMap<Continent, EnumMap<Country, List<City>>> {
+        val structure = EnumMap<Continent, EnumMap<Country, List<City>>>(Continent::class.java)
+        Continent.entries.forEach { continent ->
+            val countriesInContinent = EnumMap<Country, List<City>>(Country::class.java)
+            Country.entries
+                .filter { it.continentCode == continent.continentCode }
+                .forEach { country ->
+                    val citiesInCountry = City.entries
+                        .filter { it.countryCode == country.countryCode }
+                        .toList()
+                    countriesInContinent[country] = citiesInCountry
+                }
+            structure[continent] = countriesInContinent
+        }
+        return structure
     }
 }
