6639f8bc72a15daa2e7bd45a52748a3f77e4441a
안영길 <nilgil183@gmail.com>
2024-09-28 10:09:54 +0900

    fix: 스웨거 운영서버 서버 설정 수정

--- withaeng-api/src/main/kotlin/com/withaeng/api/config/SwaggerConfig.kt
+++ withaeng-api/src/main/kotlin/com/withaeng/api/config/SwaggerConfig.kt
@@ -2,7 +2,13 @@ package com.withaeng.api.config
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
 import io.swagger.v3.oas.annotations.security.SecurityScheme
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.context.annotation.Profile
 
 @Configuration
 @SecurityScheme(
@@ -11,4 +17,29 @@ import org.springframework.context.annotation.Configuration
     bearerFormat = "JWT",
     scheme = "bearer"
 )
-class SwaggerConfig
+class SwaggerConfig {
+
+    @Bean
+    fun defaultSwagger(): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("Withaeng API")
+                    .version("1.0")
+            )
+    }
+
+    @Profile("prod")
+    @Primary
+    @Bean
+    fun prodSwagger(defaultSwagger: OpenAPI): OpenAPI {
+        return defaultSwagger
+            .servers(
+                listOf(
+                    Server()
+                        .url("https://api.withaeng.com")
+                        .description("Production server"),
+                )
+            )
+    }
+}
